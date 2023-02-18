da37b374165cc23ab199790aa32da576525e356a
Vladislav <batyrev.vr@yandex.ru>
2023-02-18 17:37:18 +0300

    added logging

--- app.py
+++ app.py
@@ -1,35 +1,30 @@
 from flask import Flask
 import keyboard
 from os import path
-from kettle import ElectricKettle
-import db
+from kettle import ElectricKettle, logging
 
 app = Flask(__name__)
 basedir = path.abspath(path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
     path.join(basedir, 'kettle_states.db')
 
-# создаем чайник
-kettle = ElectricKettle()
-
 def main():
-    # запишем в базу данных начальное состояние чайника
-    db.create_state(kettle)
-    print(f'\nТекущая температура: {kettle.get_temperature()} °C\n')
-    print(kettle)
+    # создаем (берем в руки) чайник
+    kettle = ElectricKettle()
+    logging.info("Чайник в руках")
+    logging.info(kettle)
     # наливаем воду
     try:
         water_amount = float(input(f'Налейте воду (введите объем в литрах): '))
     except ValueError:
-        print('Введено некорректное значение, вода не налита')
+        logging.error('Введено некорректное значение, вода не налита')
         return
+    water_amount = kettle.get_water_amount() + water_amount
     kettle.set_water_amount(water_amount)
     print('Нажмите "P" (Power) для включения/выключения чайника')
     keyboard.wait('p')
     # включаем чайник
     kettle.turn_on()
-    # запишем в базу данных состояние чайника после нагревания
-    db.create_state(kettle)
     kettle.cooling()
 
 
--- kettle.py
+++ kettle.py
@@ -1,5 +1,12 @@
 import time, keyboard
 from config import *
+import logging
+
+file_log = logging.FileHandler('logging.log', encoding='utf-8')
+console_out = logging.StreamHandler()
+logging.basicConfig(format = u'%(levelname)-8s [%(asctime)s] %(message)s',
+                    level = logging.DEBUG,
+                    handlers=(file_log, console_out))
 
 class ElectricKettle:
     def __init__(self):
@@ -11,63 +18,67 @@ class ElectricKettle:
         self.is_on = False
 
     def __str__(self):
-        return  f"Характеристики чайника:\n" \
-                f"Допустимый объем воды в чайнике: от {self.min_water_amount} до {self.max_water_amount} л\n" \
-                f"Время закипания: {self.boiling_time} сек\n\n" \
+        return  f"\nХарактеристики чайника:\n" \
+                f" - Допустимый объем воды в чайнике: от {self.min_water_amount} до {self.max_water_amount} л\n" \
+                f" - Время закипания: {self.boiling_time} сек\n" \
                 f"Текущее состояние:\n" \
-                f"Количество воды: {self.water_amount} л\n" \
-                f"Текущая температура: {self.temperature} °C\n" \
-                f"Режим: {'включен' if self.is_on else 'выключен'}\n"
+                f" - Количество воды: {self.water_amount} л\n" \
+                f" - Текущая температура: {self.temperature} °C\n" \
+                f" - Режим: {'включен' if self.is_on else 'выключен'}"
 
     def turn_on(self):
         self.is_on = True
-        print('Чайник включен')
+        logging.info('Чайник включен')
         keyboard.add_hotkey('p', self.turn_off)
         self.boil()
 
     def turn_off(self):
         keyboard.remove_all_hotkeys()
         self.is_on = False
-        print('Чайник выключен')
+        logging.info('Чайник выключен')
 
     def boil(self):
-        print('Чайник нагревается')
+        logging.info('Чайник нагревается')
         # температура линейно увеличивается c AMBIENT_TEMPERATURE до BOILING_TEMPERATURE
-        step = (BOILING_TEMPERATURE - AMBIENT_TEMPERATURE) / self.boiling_time
-        for temperature in range(AMBIENT_TEMPERATURE, BOILING_TEMPERATURE + 1, int(step)):
+        step = int((BOILING_TEMPERATURE - AMBIENT_TEMPERATURE) / self.boiling_time)
+        for temperature in range(AMBIENT_TEMPERATURE, BOILING_TEMPERATURE, step):
             if self.is_on is False:
                 return
             self.set_temperature(temperature)
             time.sleep(1)
-        print(f'Чайник вскипел, температура: {self.temperature} °C')
+        self.set_temperature(BOILING_TEMPERATURE)
+        logging.info(f'Чайник вскипел, температура: {self.temperature} °C')
         self.turn_off()
 
     def cooling(self):
-        print('Чайник остывает')
+        logging.info('Чайник остывает')
         start_temperature = self.temperature
         if start_temperature != AMBIENT_TEMPERATURE:
             # температура линейно уменьшается c start_temperature до AMBIENT_TEMPERATURE
-            step = (BOILING_TEMPERATURE - AMBIENT_TEMPERATURE) / self.boiling_time
-            for temperature in reversed(range(AMBIENT_TEMPERATURE, start_temperature, int(step))):
+            step = int((BOILING_TEMPERATURE - AMBIENT_TEMPERATURE) / self.boiling_time)
+            for temperature in reversed(range(AMBIENT_TEMPERATURE, start_temperature, step)):
                 self.set_temperature(temperature)
                 time.sleep(1)
-        print(f'Чайник остыл, температура: {self.temperature} °C')
+        logging.info(f'Чайник остыл, температура: {self.temperature} °C')
 
     def set_water_amount(self, water_amount):
         # проверяем, что объем воды лежит в нужном диапазоне
         if water_amount > self.max_water_amount:
-            print(f'Воды в чайнике не может быть больше {self.max_water_amount} л, лишняя вода пролилась')
+            logging.warning(f'Воды в чайнике не может быть больше {self.max_water_amount} л, лишняя вода пролилась')
             self.water_amount = self.max_water_amount
             return
         elif water_amount <= self.min_water_amount:
-            print(f'Введено некорректное значение, вода не налита')
+            logging.error(f'Введено некорректное значение, вода не налита')
             exit()
         self.water_amount = water_amount
-        print(f'Новый объем воды в чайнике: {self.water_amount} л')
+        logging.info(f'Новый объем воды в чайнике: {self.water_amount} л')
 
     def set_temperature(self, temperature):
         self.temperature = temperature
-        print(f'Текущая температура: {self.temperature} °C')
+        logging.debug(f'Текущая температура: {self.temperature} °C')
 
     def get_temperature(self):
         return self.temperature
+    
+    def get_water_amount(self):
+        return self.water_amount
